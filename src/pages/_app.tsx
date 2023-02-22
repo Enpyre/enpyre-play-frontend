81c400ea31b213adf64e937849eb0110c90ae767
Lucas Amoêdo <lucas.advc@gmail.com>
2023-02-21 22:55:21 -0300

    Add header to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+
 import { AuthProvider } from '@/contexts/auth';
 import { ToggleThemeProvider } from '@/contexts/toggle-theme';
 import { useTheme } from '@/hooks/theme';
 import GlobalStyles from '@/view/styles.global';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
   const { theme, toggleTheme } = useTheme();
@@ -16,6 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <AuthProvider>
+          <Head>
+            <title>Enpyre - A Python game engine in React</title>
+          </Head>
           <Component {...pageProps} />
         </AuthProvider>
       </ToggleThemeProvider>
